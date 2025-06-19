836033d9d43e235e6a87ab7e8846c2ed33ed8091
marcelaobeso <march.obeso@gmail.com>
2025-06-19 19:48:08 +0100

    feat: added about section and content file

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,17 @@
 import os
+import json
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
 
+
 load_dotenv()
 app = Flask(__name__)
+with open('./app/static/content/content.json', 'r', encoding='utf-8') as file:
+    content = json.load(file)
+
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    return render_template('index.html', title=content["title"], about=content["about"]["content"], url=os.getenv("URL"))
+
