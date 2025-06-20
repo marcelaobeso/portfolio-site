dbd9e1616fc65ec813d2cb4bbee6ed690ad15892
ManyColours <124391674+OnTheKeys@users.noreply.github.com>
2025-06-20 15:08:11 +0100

    Add photo and education (#10)
    * Added a placeholder for education. A class to make creation education entries easier, added placeholder education entry, added placeholder profile picture
    
    * Fixed issue where module would not be found
    
    * made 'end_date' variable optional with a default value of present

--- app/__init__.py
+++ app/__init__.py
@@ -2,16 +2,32 @@ import os
 import json
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
-
+from app.education import education_entry
 
 load_dotenv()
 app = Flask(__name__)
 with open('./app/static/content/content.json', 'r', encoding='utf-8') as file:
     content = json.load(file)
 
+edu_1_json = content["education"]["entry 1"]
 
+edu_1 = education_entry(edu_1_json["name"], 
+                        edu_1_json["degree"], 
+                        edu_1_json["start_date"], 
+                        edu_1_json["end_date"], 
+                        edu_1_json["grade"], 
+                        edu_1_json["skills"])
 
 @app.route('/')
 def index():
-    return render_template('index.html', title=content["title"], about=content["about"]["content"], url=os.getenv("URL"))
-
+    return render_template('index.html', 
+                           title=content["title"], 
+                           about=content["about"]["content"], 
+                           picture=content["picture"],
+                           institute_name=edu_1.name,
+                           degree=edu_1.degree,
+                           start_date=edu_1.start_date,
+                           end_date=edu_1.end_date,
+                           grade=edu_1.grade,
+                           skills =edu_1.skills,
+                           url=os.getenv("URL"))
---
+++ app/education.py
@@ -0,0 +1,8 @@
+class education_entry:
+    def __init__(self, name, degree, start_date, end_date="Present", grade=None, skills=None):
+        self.name = name
+        self.degree = degree
+        self.start_date = start_date
+        self.end_date = end_date
+        self.grade = grade
+        self.skills = skills
