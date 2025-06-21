8db9474f40d95d665d98b8f521e766c109e8a670
marcelaobeso <march.obeso@gmail.com>
2025-06-21 15:40:14 +0100

    feat: added section with map of visited places

--- app/__init__.py
+++ app/__init__.py
@@ -4,13 +4,14 @@ from flask import Flask, render_template, request
 from dotenv import load_dotenv
 from app.education import education_entry
 from app.hobby import hobby_entry
+from app.country import country_entry
 
 
 load_dotenv()
 app = Flask(__name__)
 with open('./app/static/content/content.json', 'r', encoding='utf-8') as file:
     data = json.load(file)
-    about, title, hobbies_list, picture = data["about"], data["title"], data["hobbies-list"], data["picture"]
+    about, title, hobbies_list, picture, travels = data["about"], data["title"], data["hobbies-list"], data["picture"], data["travels"]
 
 edu_1_json = data["education"]["entry 1"]
 
@@ -25,6 +26,10 @@ hobbies_items = []
 for hobby in hobbies_list:
     hobbies_items.append(hobby_entry(**hobby))
 
+country_list = []
+for country in travels:
+    country_list.append(country_entry(**country))
+
 
 @app.route('/')
 def index():
@@ -38,6 +43,7 @@ def index():
                            end_date=edu_1.end_date,
                            grade=edu_1.grade,
                            skills =edu_1.skills,
+                           countries=country_list,
                            url=os.getenv("URL"))
 
 @app.route('/hobbies', methods=['GET'])
