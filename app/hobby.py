8f87401321c991425f46e87071d993fc2f21aca2
Marcela Obeso <94816900+marcelaobeso@users.noreply.github.com>
2025-07-12 22:23:41 +0100

    feat: created db connection and timeline endpoint (#24)

--- app/hobby.py
+++
@@ -1,5 +0,0 @@
-class hobby_entry:
-    def __init__(self, img_url, name, description=""):
-        self.img_url = img_url
-        self.name = name
-        self.description = description
--- app/job.py
+++
@@ -1,8 +0,0 @@
-class job_entry:
-    def __init__(self, title, company_name, start_date, description, skills, end_date="Present"):
-        self.title = title
-        self.company_name = company_name
-        self.start_date = start_date
-        self.end_date = end_date
-        self.description = description
-        self.skills = skills
