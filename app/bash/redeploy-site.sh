44514643eaf51524e7f4a2f3feb19e62604fb3b3
OnTheKeys <oriasotr@tcd.ie>
2025-07-27 14:39:37 +0000

    Updated redeploy-site.sh

--- app/bash/redeploy-site.sh
+++ app/bash/redeploy-site.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd /root/portfolio-site
-git fetch && git reset origin/main --hards
+git fetch && git reset origin/main --hard
 docker compose -f docker-compose.prod.yml down
 docker compose -f docker-compose.prod.yml up -d --build
